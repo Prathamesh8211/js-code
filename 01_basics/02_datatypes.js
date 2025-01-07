b8a9ca460f881ef0795c62cb58c076df445e15d7
Prathamesh8211 <suryavanshiprathamesh8211@gmail.com>
2025-01-07 12:12:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let name="Prathamesh"
+let age=21
+
+let state;
+let isloggedin = false
+
+let n=null
+
+console.log(typeof age)
+
+console.log(typeof name)
+
+console.log(typeof state)
+
+console.log(typeof n)
