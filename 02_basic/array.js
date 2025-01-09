837417ef7dd24687ac34f59a3bc28c2d8db80043
Prathamesh8211 <suryavanshiprathamesh8211@gmail.com>
2025-01-09 05:44:47 +0000

    array

---
+++ 02_basic/array.js
@@ -0,0 +1,13 @@
+const myarr= [1,2,3,4,5,6]
+
+// console.log(myarr)
+
+// myarr.push(7)
+
+// console.log(myarr)
+
+// myarr.pop()
+// console.log(myarr)
+
+const newarr= myarr.join()
+console.log(newarr);
---
+++ 02_basic/objects.js
@@ -0,0 +1,16 @@
+const Jsuser = {
+    name: "Prathamesh",
+    "full name": "Prathamesh Suryavanshi",
+    age : 21,
+    city : "kolhapur",
+    is_loggedin : false
+}
+
+// console.log(Jsuser);
+
+console.log(Jsuser["full name"])
+console.log(Jsuser["age"]);
+
+
+
+
